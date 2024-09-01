09e5ab120ddc2f907a152ff4b267d220ee24ef96
Shuvam BC <81623225+bcShuvam@users.noreply.github.com>
2024-09-01 11:49:38 +0545

    1st commit: Getting started

--- lib/models/envioment.dart
+++ lib/models/envioment.dart
@@ -8,16 +8,4 @@ class Enviroment {
   static String get id {
     return dotenv.get('KEY_ID', fallback: 'KEY not available');
   }
-
-  static String get counter {
-    return dotenv.get('KEY_COUNTER', fallback: 'KEY not available');
-  }
-
-  static String get email {
-    return dotenv.get('KEY_EMAIL', fallback: 'KEY not available');
-  }
-
-  static String get password {
-    return dotenv.get('KEY_PASSWORD', fallback: 'KEY not available');
-  }
 }
